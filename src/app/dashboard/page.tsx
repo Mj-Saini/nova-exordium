4f9fae562e595b2596e632f032da77fd41937116
mj-saini <manojsaini97285@gmail.com>
2025-03-21 11:44:37 +0530

    admin header props

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -43,7 +43,7 @@ const DashboardPage = () => {
         </div>
         <div className=" w-full md:w-[calc(100%-200px)] lg:w-[calc(100%-296px)]  px-2.5 lg:px-5">
           <div className="top-0 sticky z-10">
-            <AdminHeader />
+            <AdminHeader heading="dashboard" />
           </div>
           <div className="flex flex-wrap lg:mt-2 -mx-2.5 xl:-mx-3">
             {[1, 1, 1, 1].map((items, index) => (
